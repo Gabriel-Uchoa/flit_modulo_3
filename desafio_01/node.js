a1ee7db35f1e1ca301f9e89b51d17b1a63bf68e0
Gabriel-Uchoa <uchoagabriely@hotmail.com>
2022-11-30 17:09:02 -0300

    Html consumindo json

--- desafio_01/node.js
+++ desafio_01/node.js
@@ -7,12 +7,7 @@ readXlsxFile('./data/Carros.xlsx').then((rows) => {
     for (let x = 1; x < rows.length; x++) {
         var [marca, modelo, ano, placa, cor, imagem] = [rows[x][0], rows[x][1], rows[x][2], rows[x][3], rows[x][4], rows[x][5]]
         const objToArray = {
-            marca,
-            modelo,
-            ano,
-            placa,
-            cor,
-            imagem
+            marca, modelo, ano, placa, cor, imagem
         }
         obj.push(objToArray)
     }
--- desafio_01/scripts/index.js
+++ desafio_01/scripts/index.js
@@ -1,5 +1,61 @@
+const img_carousel = document.getElementById('img_carousel')
+const arrow_right = document.getElementById('arrowRight')
+const describes = document.querySelectorAll('span')
+let contador = 0
 fetch("./carros.json")
     .then(response => {
         return response.json();
     })
-    .then(jsondata => console.log(jsondata));
+    .then(jsondata => nextCard(jsondata, contador));
+function addContador() {
+    contador = contador + 1;
+    console.log(contador)
+}
+function subContador() {
+    contador = contador - 1;
+    console.log(contador)
+}
+function verificarContador(num) {
+    if (num > 4) {
+        contador = 0;
+    }
+    if (num === -1) {
+        contador = 5;
+    }
+}
+function nextCard(data, position) {
+
+    for (let i = 0; i < describes.length; i++) {
+        let element = document.getElementById(describes[i].id)
+        element.textContent = data[position][describes[i].id]
+    }
+    img_carousel.src = data[position]["imagem"]
+}
+function anterCard(data, position) {
+    for (let i = 0; i < describes.length; i++) {
+        let element = document.getElementById(describes[i].id)
+        element.textContent = data[position][describes[i].id]
+    }
+    img_carousel.src = data[position]["imagem"]
+
+}
+
+
+function prox() {
+    verificarContador(contador)
+    addContador()
+    fetch("./carros.json")
+        .then(response => {
+            return response.json();
+        })
+        .then(jsondata => nextCard(jsondata, contador));
+}
+function ante() {
+    verificarContador(contador)
+    subContador()
+    fetch("./carros.json")
+        .then(response => {
+            return response.json();
+        })
+        .then(jsondata => anterCard(jsondata, contador));
+}
