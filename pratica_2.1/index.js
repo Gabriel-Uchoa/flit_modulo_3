60bf384722e4e75190263b2afc2da9b8e81ecd6c
Gabriel-Uchoa <uchoagabriely@hotmail.com>
2022-12-01 23:01:55 -0300

    renomeando arquivo js

--- pratica_2.1/index.js
+++ pratica_2.1/index.js
@@ -1,4 +1,4 @@
-const calc = require('./CalcConsumeFuel')
+const calc = require('./calcTravel')
 
 console.log("Litros de Combustivel Consumidos: ", calc.fuelConsume(32000, "gasolina"))
 console.log("Número de paradas: ", calc.numberStops([0, 1, 1, 1], 4))
