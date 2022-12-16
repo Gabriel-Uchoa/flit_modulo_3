81ddc68bbd7eb29576b7331cdaf3eda0d4a839d1
Gabriel-Uchoa <uchoagabriely@hotmail.com>
2022-12-15 22:51:20 -0300

    Pratica 5.2 concluida

--- pratica_5.2/index.js
+++ pratica_5.2/index.js
@@ -4,6 +4,12 @@ async function searchStudentId(id) {
     return await data;
 }
 
+async function searchStudents() {
+    const response = await fetch(`https://apigenerator.dronahq.com/api/g4C15xPP/students`);
+    let data = await response.json();
+    return await data;
+}
+
 async function searchTaskPerIdGrades(grades) {
     const tasks = [];
     for (let i = 0; i < grades.length; i++) {
@@ -14,6 +20,14 @@ async function searchTaskPerIdGrades(grades) {
             })
     }
     return tasks
+}
+
+async function searchTasks() {
+    return await fetch(`https://apigenerator.dronahq.com/api/75U0yEKU/tasks`)
+        .then(async function (response) {
+            let body = await response.json()
+            return body
+        })
 }
 
 async function searchGradesPerStudentId(id) {
@@ -29,6 +43,11 @@ async function searchGradesPerStudentId(id) {
         })
     return grades
 }
+async function searchGrades() {
+    const response = await fetch(`https://apigenerator.dronahq.com/api/5Bba_f-L/grades`);
+    let data = await response.json();
+    return await data;
+}
 
 async function findAllActivitiesPerStudent(idStudent) {
     const student = await searchStudentId(idStudent)
@@ -36,12 +55,84 @@ async function findAllActivitiesPerStudent(idStudent) {
     const tasks = await searchTaskPerIdGrades(grades)
 
     console.log(`Aluno: ${student.Name}`)
+
     for (let i = 0; i < grades.length; i++) {
         console.log(`Materia: ${tasks[i].title} - Nota: ${grades[i].number}`)
     }
-    // return student.Name
+}
+
+async function gradeAveragePerStudent(idStudent) {
+    var average = 0
+    const student = await searchStudentId(idStudent)
+    const grades = await searchGradesPerStudentId(idStudent)
+
+    for (let i = 0; i < grades.length; i++) {
+        average += parseFloat(grades[i].number)
+    }
+
+    console.log(`Aluno: ${student.Name}`)
+    console.log(`Média do Aluno: ${average / grades.length}`)
+}
+
+async function searchTasksPending(idStudent) {
+    const grades = await searchGradesPerStudentId(idStudent)
+    const tasksStudent = await searchTaskPerIdGrades(grades)
+    const tasks = await searchTasks()
+
+    const tasksPendig = tasks.filter(x => {
+        return JSON.stringify(tasksStudent).indexOf(JSON.stringify(x)) < 0;
+    });
+    const tasksDelivered = tasks.filter(x => {
+        return JSON.stringify(tasksStudent).indexOf(JSON.stringify(x)) > 0;
+    });
+    for (let i = 0; i < tasksPendig.length; i++) {
+        console.log(`Materia Pendente: ${tasksPendig[i].title}`)
+    }
+    for (let i = 0; i < tasksDelivered.length; i++) {
+        console.log(`Materia Concluida: ${tasksDelivered[i].title}`)
+    }
+}
+
+async function searchStudentsPerDeliveredOfTasks(task) {
+    const tasks = await searchTasks()
+    const students = await searchStudents()
+    const grades = await searchGrades()
+
+    var idTask = 0
+    var taskStudentIds = []
+    var studentTaskDelivered = []
+
+    tasks.forEach(element => {
+        if (element.title == task) {
+            idTask = element.id
+        }
+    });
+
+    grades.forEach(element => {
+        if (element.taskId == idTask) {
+            taskStudentIds.push(element)
+        }
+    });
+
+    students.forEach(element => {
+        taskStudentIds.forEach(x => {
+            if (element.id == x.studentId) {
+                studentTaskDelivered.push(element)
+            }
+        })
+    });
+
+    for (let i = 0; i < taskStudentIds.length; i++) {
+        for (let x = 0; x < studentTaskDelivered.length; x++) {
+            if (taskStudentIds[i].studentId == studentTaskDelivered[x].id) {
+                console.log(`Nome: ${studentTaskDelivered[x].Name} - Nota: ${taskStudentIds[i].number}`)
+            }
+        }
+    }
 }
 
 var findAllActivitiesPerStudent = findAllActivitiesPerStudent(5)
+var gradeAveragePerStudent = gradeAveragePerStudent(5)
+var searchTasksPending = searchTasksPending(5)
+var searchStudentsPerDeliveredOfTasks = searchStudentsPerDeliveredOfTasks("Colocando em prática 2.3")
 
-//findAllActivitiesPerStudent.then(console.log)
